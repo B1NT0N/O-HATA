db1a62f47eae29e3b5d9ad4db9a354e8f753e7d2
B1NT0N <kleiton1002andrade@gmail.com>
2024-01-31 17:27:10 -0100

    Main UI OK

--- main.py
+++ main.py
@@ -1,49 +1,220 @@
 import flet as ft
 import time
 
+import flet as ft
+
 def main(page):
+    page.window_min_height = 650        # window's width is 200 px
+    page.window_min_width = 600       # window's height is 200 px
+    # page.window_resizable = True  # window is not resizable
 
-    page.theme = ft.Theme(
-    color_scheme=ft.ColorScheme(
-        primary=ft.colors.GREEN,
-        primary_container=ft.colors.GREEN_200
-        # ...
-    ),
-)
-
-    def add_clicked():
-        ft.Text(value="12", color="green"),
-
-    parameter = ft.TextField(hint_text="Number", width=200)
-
-    unity_dropdown = ft.Dropdown(
-        width=100,
-        options=[
-            ft.dropdown.Option("Watts"),
-            ft.dropdown.Option("Log"),
-        ],
-    )
+    page.title = "O-HAMA"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     page.add(
-        
+        ft.Container(
+            content=ft.Column(
+                [
+                    ft.Container(
+                        alignment=ft.alignment.center,
+                        margin=10,
+                        padding=10,
+                        content=ft.Text(
+                            "O-HAMA",
+                            theme_style=ft.TextThemeStyle.DISPLAY_SMALL,
+                            weight=ft.FontWeight.BOLD,
 
-        ft.Text(value="Frequency", color="green"),
-        ft.Row([
-            ft.TextField(hint_text="150 - 2000 MHz", width=200),
-                ]),
+                            )
+                    ),
 
-        ft.Text(value="Distance", color=""),
-        ft.Row([
-            ft.TextField(hint_text="1 - 20 Km", width=200),
-            unity_dropdown
-                ]),
+                    ft.Container(
+                        alignment=ft.alignment.center,
+                        margin=ft.margin.symmetric(horizontal=20),
+                        padding=ft.padding.symmetric(horizontal=15),
+                        
+                        content=ft.Card(
 
-        ft.Text(value="Mobile Station Heigh", color=""),
-        ft.Row([
-            ft.TextField(hint_text="1 - 10 m", width=200),
-            unity_dropdown
-            ]),
- 
+                            content=ft.Row(
+                                expand=True,
+                                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+
+                                controls=[
+                                    
+                                    ft.Container(
+                                        content=ft.Text(
+                                            "Freq.",
+                                            theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
+                                            weight=ft.FontWeight.BOLD,
+                                            color=ft.colors.BLUE_200,
+                                            ),
+                                        margin=10,
+                                        padding=10,
+                                        alignment=ft.alignment.center,
+                                        ),
+                                        
+                                    ft.Container(
+                                        content= ft.TextField(
+                                        label = "Frequency",
+                                        hint_text="150 - 2000 ",
+                                        input_filter=ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
+                                        keyboard_type="NUMBER",
+                                        suffix_text = " MHz",
+                                        text_align = "RIGHT",
+                                        # border=ft.InputBorder.UNDERLINE,
+                                        # filled=False,
+                                        ),
+
+                                        margin=5,
+                                        padding=10,
+                                        # alignment=ft.alignment.center_right,
+                                    ) 
+                                ]
+                                
+                            ),
+                            
+                        ),
+
+                        # content = ft.ListTile(
+                        #     leading=ft.Icon(ft.icons.ALBUM),
+                        #     title=ft.Text("The Enchanted Nightingale"),
+                        #     subtitle=ft.Text(
+                        #         "Music by Julie Gable. Lyrics by Sidney Stein."
+                        #     ),
+                        # ),
+                        
+                    ),
+                    ft.Container(
+                        alignment=ft.alignment.center,
+                        margin=ft.margin.symmetric(horizontal=20),
+                        padding=ft.padding.symmetric(horizontal=15),
+                        
+                        content=ft.Card(
+
+                            content=ft.Row(
+                                expand=True,
+                                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+
+                                controls=[
+                                    
+                                    ft.Container(
+                                        content=ft.Text(
+                                            "Dist.",
+                                            theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
+                                            weight=ft.FontWeight.BOLD,
+                                            color=ft.colors.BLUE_200,
+                                            ),
+                                        margin=10,
+                                        padding=10,
+                                        alignment=ft.alignment.center,
+                                        ),
+                                        
+                                    ft.Container(
+                                        content= ft.TextField(
+                                        label = "Distance",
+                                        hint_text="1 - 20 ",
+                                        input_filter=ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
+                                        keyboard_type="NUMBER",
+                                        suffix_text = " Km",
+                                        text_align = "RIGHT",
+                                        # border=ft.InputBorder.UNDERLINE,
+                                        # filled=False,
+                                        ),
+
+                                        margin=5,
+                                        padding=10,
+                                        # alignment=ft.alignment.center_right,
+                                    ) 
+                                ]
+                                
+                            ),
+                            
+                        ),
+
+                        # content = ft.ListTile(
+                        #     leading=ft.Icon(ft.icons.ALBUM),
+                        #     title=ft.Text("The Enchanted Nightingale"),
+                        #     subtitle=ft.Text(
+                        #         "Music by Julie Gable. Lyrics by Sidney Stein."
+                        #     ),
+                        # ),
+                        
+                    ),
+                    ft.Container(
+                        alignment=ft.alignment.center,
+                        margin=ft.margin.symmetric(horizontal=20),
+                        padding=ft.padding.symmetric(horizontal=15),
+                        
+                        content=ft.Card(
+
+                            content=ft.Row(
+                                expand=True,
+                                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+
+                                controls=[
+                                    
+                                    ft.Container(
+                                        content=ft.Text(
+                                            "Hgt.",
+                                            theme_style=ft.TextThemeStyle.HEADLINE_SMALL,
+                                            weight=ft.FontWeight.BOLD,
+                                            color=ft.colors.BLUE_200,
+                                            ),
+                                        margin=10,
+                                        padding=10,
+                                        alignment=ft.alignment.center,
+                                        ),
+                                        
+                                    ft.Container(
+                                        content= ft.TextField(
+                                        label = "Height",
+                                        hint_text="1 - 10 ",
+                                        input_filter=ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
+                                        keyboard_type="NUMBER",
+                                        suffix_text = " m",
+                                        text_align = "RIGHT",
+                                        # border=ft.InputBorder.UNDERLINE,
+                                        # filled=False,
+                                        ),
+
+                                        margin=5,
+                                        padding=10,
+                                        # alignment=ft.alignment.center_right,
+                                    ) 
+                                ]
+                                
+                            ),
+                            
+                        ),
+
+                        # content = ft.ListTile(
+                        #     leading=ft.Icon(ft.icons.ALBUM),
+                        #     title=ft.Text("The Enchanted Nightingale"),
+                        #     subtitle=ft.Text(
+                        #         "Music by Julie Gable. Lyrics by Sidney Stein."
+                        #     ),
+                        # ),
+                        
+                    ),
+
+                    ft.Container(
+                        margin = ft.margin.symmetric(horizontal=100),
+                        alignment=ft.alignment.center,
+                        content=ft.ElevatedButton(
+                            
+                            "Calculate",
+                            bgcolor=ft.colors.BLUE_800,
+                            color=ft.colors.WHITE,
+                            style=ft.ButtonStyle(
+                            shape=ft.RoundedRectangleBorder(radius=10),
+                            ),
+                            width=2050,
+                            height=50,
+                            )
+                    )
+                ]
+            )
         )
+        
+    )
 
-ft.app(target=main)   
+ft.app(target=main, )
